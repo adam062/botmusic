3da8afe7082cc12d7dcc87132a8d133b92200fb7
adam062 <80047476+adam062@users.noreply.github.com>
2022-10-29 00:13:08 +0200

    Update index.js

--- index.js
+++ index.js
@@ -57,7 +57,7 @@ i18n.configure({
  */
 client.on("ready", () => {
   console.log(`${client.user.username} ready!`);
-  client.user.setActivity { type: "PLAYING" };
+  client.user.setActivity (`/help`,{ type: "PLAYING" });
   client.user.setStatus("online");
   let channel = client.channels.cache.find(r => r.id === "1015575482969890926");//ايدي الروم
   if(!channel)return console.log("can't find channel")
